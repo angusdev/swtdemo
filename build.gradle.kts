c8d32280ebab40bc2a37ff2526cabff93752dfab
Angus <angusdev@gmail.com>
2022-02-11 21:10:12 +0800

    Use Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    application
+    eclipse
+}
+
+repositories {
+    mavenCentral()
+}
+
+val osgi_platform = "cocoa.macosx.x86_64"
+val swt_version = "3.118.0"
+
+dependencies {
+    //testImplementation("org.testng:testng:7.4.0")
+    //implementation("com.google.guava:guava:30.1.1-jre")
+    implementation("org.apache.pdfbox:pdfbox:2.0.25")
+    implementation("org.apache.commons:commons-csv:1.9.0")
+    implementation("org.eclipse.platform:org.eclipse.swt.cocoa.macosx.x86_64:3.118.0")
+}
+
+configurations.all {
+    resolutionStrategy {
+        dependencySubstitution {
+            // The maven property ${osgi.platform} is not handled by Gradle
+            // so we replace the dependency, using the osgi platform from the project settings
+            substitute(module("org.eclipse.platform:org.eclipse.swt.\${osgi.platform}")).using(module("org.eclipse.platform:org.eclipse.swt.$osgi_platform:$swt_version"))
+        }
+    }
+}
+
+application {
+    mainClass.set("org.ellab.swt.demo.SwtDemo")
+    applicationDefaultJvmArgs = listOf("-XstartOnFirstThread")
+}
+
+java.sourceSets["main"].java {
+    srcDir("src/main")
+}
+
+tasks.withType<ProcessResources> {
+    from("src/main")
+    include("**/*.ico")
+}
